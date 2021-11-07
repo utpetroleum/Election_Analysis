dfe817917e6af7fc785f0eb374f899977d9c3baf
utpetroleum <quang.nguyen@utexas.edu>
2021-11-06 20:41:52 -0500

    Adding the election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+if "Arapahoe" in counties and "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapahoe or El Paso is not in the list of counties.")
+
+
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapahoe or El Paso is not in the list of counties.") 
+
+for county in counties:
+    print(county)    
